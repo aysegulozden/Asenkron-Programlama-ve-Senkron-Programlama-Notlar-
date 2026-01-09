809be43e6490c1605d3a6efc9ddb3e6c123474fe
AysegulOzden <ozdenaysegul728@gmail.com>
2026-01-09 20:15:04 +0300

    PROMİSE+XMLRequest kullanımı

---
+++ promise2.js
@@ -0,0 +1,27 @@
+//? PROMİSE+XMLHTTPREQUEST 
+
+function readStudent(url){
+    return new Promise((resolve,reject)=>{
+        const xhr= new XMLHttpRequest();
+        try {
+            xhr.addEventListener("readystatechange",()=>{
+            if(xhr.readyState ===4 && xhr.status===200){
+                resolve(JSON.parse(xhr.responseText))
+            }
+        })
+        } catch (error) {
+            reject(error)
+        }
+
+        xhr.open("GET",url)
+        xhr.send();
+    })
+}
+
+readStudent("student.json")
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>{
+    console.log(error)
+})
