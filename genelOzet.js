37224f368801a8843d25577a6020dcdd0782cc5d
AysegulOzden <ozdenaysegul728@gmail.com>
2026-01-11 12:00:47 +0300

    Genel Ozet

---
+++ genelOzet.js
@@ -0,0 +1,23 @@
+//?----------------------- ASENKRON VE SENKRON PROGRAMLAMA GENEL ÖZET -------
+/*
+
+Senkron : Kodların sırayla çalışması demektir.
+Asenkron : Aynı zamanda birden fazla kodun çalışabilmesi demektir
+
+JAVASCRİPT SENKRON ÇALIŞAN BİR PROGRAMLAMA DİLİDİR. Yani kodlar yukardıdan aşağıdaya derleyerek sırayla okunur.
+anca bazı durumlarda asenkron çalışabilir. Sebep olan durumlar:
+1-Timing():zamansal
+2-Eventler:olaylar
+3-HTTP İstekleri 
+  XMLHttpRequest
+  fetch API
+  Axios (REACT DA KULLANILIR)
+
+Asenkron yapıları yönetmek için 3 temel yapı var:
+1-Callback -ES6 ÖNCESİ
+2-Promise  -ES6 İLE GELDİ 2015 YILINDA
+3-Async await -ES7 İLE GELDİ  
+ 
+
+
+*/
